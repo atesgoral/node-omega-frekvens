caff38247fabe4a50538e66d239b9f36f3422514
Ates Goral <ates.goral@shopify.com>
2020-03-21 14:33:33 -0400

    WebSockets

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
+const socketIoClient = require('socket.io-client');
+
 const frekvens = require('./build/Release/binding');
+
+dotenv.config();
 
 frekvens.start((event) => {
   console.log('event:', event);
@@ -7,15 +12,19 @@ frekvens.start((event) => {
 const pixels = new Uint8Array(16 * 16);
 const buffer = Buffer.from(pixels.buffer);
 
+let renderFn = function (pixels, t) {
+  const x = Math.cos(t) * 8 + 8 | 0;
+  const y = Math.sin(t) * 8 + 8 | 0;
+
+  pixels[y * 16 + x] = 1;
+};
+
 setInterval(() => {
   pixels.fill(0);
 
   const t = Date.now() / 1000;
 
-  const x = Math.cos(t) * 8 + 8 | 0;
-  const y = Math.sin(t) * 8 + 8 | 0;
-
-  pixels[y * 16 + x] = 1;
+  renderFn(pixels, t);
 
   frekvens.render(buffer);
 }, 1000 / 60);
@@ -24,4 +33,19 @@ process.on('beforeExit', (code) => {
   frekvens.stop((event) => {
     console.log('event:', event);
   });
+});
+
+const socket = socketIoClient(process.env.WEBSOCKET_SERVER_URL);
+
+socket.on('connect', () => {
+  console.log('Connected');
+  socket.emit('identify', process.env.FREKVENS_CLIENT_SECRET);
+});
+
+socket.on('script', (script) => {
+  console.log('Script', script);
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected');
 });
