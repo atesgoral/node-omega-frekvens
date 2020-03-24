81584e4524651f663788489a7ffa8dd4bbea8340
Ates Goral <ates.goral@shopify.com>
2020-03-23 23:45:51 -0400

    Add init.d.sh

---
+++ init.d.sh
@@ -0,0 +1,15 @@
+#!/bin/sh /etc/rc.common
+
+START=10
+STOP=15
+
+start() {
+  echo "Starting FREKVENS"
+  cd /root/node-omega-frekvens
+  node index.js &
+}
+
+stop() {
+  echo "Stopping FREKVENS"
+  killall node
+}
