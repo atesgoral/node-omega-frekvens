145122d798efe2d7633879620a4f7ef6d37c046c
Ates Goral <ates.goral@shopify.com>
2020-03-27 19:06:18 -0400

    Callback indirection

--- src/FREKVENS.cpp
+++ src/FREKVENS.cpp
@@ -6,7 +6,9 @@ Renderer renderer;
 
 namespace FREKVENS {
   void start(const SwitchEventCallback switchEventCallback) {
-    renderer.start(switchEventCallback);
+    renderer.start([&switchEventCallback](const char *szEventName) -> void {
+      switchEventCallback(szEventName);
+    });
   }
 
   void render(const char *pBuffer) {
