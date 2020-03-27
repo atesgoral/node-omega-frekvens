8d5127387b300454fb3d9b73554113e1e36a36ec
Ates Goral <ates.goral@shopify.com>
2020-03-27 12:37:36 -0400

    Use atomic Boolean

--- src/Renderer.h
+++ src/Renderer.h
@@ -1,13 +1,15 @@
 #ifndef _RENDERER_H_
 #define _RENDERER_H_
 
+#include <atomic>
+
 #include <uv.h>
 
 #include "SafeBuffer.h"
 
 class Renderer {
   SafeBuffer m_safeBuffer;
-  bool m_isRunning;
+  std::atomic<bool> m_isRunning;
   uv_thread_t m_thread;
 
   static void gpioLoop(void *pArg);
